bab11fcbb2a3890aa96c85ba00dbb937c0bc0312
Ryan <yuema@callutheran.edu>
2017-01-23 01:28:18 -0800

    bind the products page with database and finish part of pagination

--- Product Sales/products.aspx.cs
+++ Product Sales/products.aspx.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 
 namespace Product_Sales
@@ -11,7 +15,20 @@ namespace Product_Sales
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            int tmp = 0;
+            if (IsPostBack)
+            {
+                Session["startId"] = Session["lastId"];
+                tmp = (int)Session["lastId"];
+            }
+            else
+            {
+                Session["startId"] = 0;
+            }
+            DataView dv = (DataView)SqlDataSource1.Select(DataSourceSelectArguments.Empty);
+            Repeater1.DataBind();
+            tmp = (dv.Table.Rows.Count == 0) ? tmp : (int)dv.Table.Rows[dv.Table.Rows.Count - 1][0];
+            Session["lastId"] = tmp;
         }
     }
 }
--- database.sql
+++ database.sql
@@ -26,7 +26,7 @@ create table products
 	price int not null,
 	discount int not null,
 	color varchar(100) not null,
-	size varchar(30) not null,
+	size varchar(100) not null,
 	catogory varchar(20) not null,
 	brand varchar(10) not null,
 	description varchar(1000) not null,
@@ -45,3 +45,18 @@ add constraint qk_id primary key (id)
 
 insert into userlogin (username, password, lastName, firstName, sex, mobile, email, address) values('mayue','mayue','Ma', 'Yue', 1, '857-285-1873', 'yuema@callutheran.edu', '1527 Patricia Ave, Simi Valley, CA')
 go
+
+insert into products (name, price, discount, color, size, catogory, brand, description, infomation, rate, numOfRate, reviews, tag, sku, picture) values('Lorem Ipsum1', 95, 50, 'Black, Red', 'Large, Medium, Small', 'Women', 'Titan', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.', '10 * 10 * 10', 4, 2, 'Good. Great.', 'Woman', 'CK09', 'p-1.png');
+insert into products (name, price, discount, color, size, catogory, brand, description, infomation, rate, numOfRate, reviews, tag, sku, picture) values('Lorem Ipsum2', 95, 50, 'Black, Red', 'Large, Medium, Small', 'Women', 'Titan', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.', '10 * 10 * 10', 4, 2, 'Good. Great.', 'Woman', 'CK09', 'p-2.png');
+insert into products (name, price, discount, color, size, catogory, brand, description, infomation, rate, numOfRate, reviews, tag, sku, picture) values('Lorem Ipsum3', 95, 50, 'Black, Red', 'Large, Medium, Small', 'Women', 'Titan', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.', '10 * 10 * 10', 4, 2, 'Good. Great.', 'Woman', 'CK09', 'p-3.png');
+insert into products (name, price, discount, color, size, catogory, brand, description, infomation, rate, numOfRate, reviews, tag, sku, picture) values('Lorem Ipsum4', 95, 50, 'Black, Red', 'Large, Medium, Small', 'Women', 'Titan', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.', '10 * 10 * 10', 4, 2, 'Good. Great.', 'Woman', 'CK09', 'p-4.png');
+insert into products (name, price, discount, color, size, catogory, brand, description, infomation, rate, numOfRate, reviews, tag, sku, picture) values('Lorem Ipsum5', 95, 50, 'Black, Red', 'Large, Medium, Small', 'Women', 'Titan', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.', '10 * 10 * 10', 4, 2, 'Good. Great.', 'Woman', 'CK09', 'p-5.png');
+insert into products (name, price, discount, color, size, catogory, brand, description, infomation, rate, numOfRate, reviews, tag, sku, picture) values('Lorem Ipsum6', 95, 50, 'Black, Red', 'Large, Medium, Small', 'Women', 'Titan', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.', '10 * 10 * 10', 4, 2, 'Good. Great.', 'Woman', 'CK09', 'p-6.png');
+insert into products (name, price, discount, color, size, catogory, brand, description, infomation, rate, numOfRate, reviews, tag, sku, picture) values('Lorem Ipsum7', 95, 50, 'Black, Red', 'Large, Medium, Small', 'Women', 'Titan', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.', '10 * 10 * 10', 4, 2, 'Good. Great.', 'Woman', 'CK09', 'p-7.png');
+insert into products (name, price, discount, color, size, catogory, brand, description, infomation, rate, numOfRate, reviews, tag, sku, picture) values('Lorem Ipsum8', 95, 50, 'Black, Red', 'Large, Medium, Small', 'Women', 'Titan', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.', '10 * 10 * 10', 4, 2, 'Good. Great.', 'Woman', 'CK09', 'p-8.png');
+go
+
+select top 3 * from products  
+where id not in (  
+  select top 2 id from products  
+)  
