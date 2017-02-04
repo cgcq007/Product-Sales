b2234ef9144508dbab96971e3dce6a550cea03f6
cgcq007 <cgcq007@gmail.com>
2017-02-03 23:49:38 -0800

    Update single.aspx.cs
    Add top watches

--- Product Sales/single.aspx.cs
+++ Product Sales/single.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,15 +11,45 @@ namespace Product_Sales
 {
     public partial class single : System.Web.UI.Page
     {
+        
         String id = "0";
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            SqlConnection conn = new SqlConnection("Data Source=localhost;Initial Catalog=productSales;Integrated Security=True");
+            conn.Open();
+
             id = Request.QueryString["id"];
+            String sql = "update products set clickTime += 1 where id =";
+            String sqlTop = "SELECT TOP (10) id, name, price, picture FROM products ORDER BY clickTime DESC";
             if (Session["cart"] == null)
             {
                 Session["cart"] = new Dictionary<String, int>();
             }
+            if (!this.IsPostBack)
+            {
+                DataSet ds = GetData(sqlTop + ";" + sql + id, conn);
+                //DataSet ds = GetData(sqlTop, conn);
+                Repeater1.DataSource = ds;
+                Repeater1.DataBind();
+
+            }
         }
+        protected DataSet GetData(String queryString, SqlConnection conn)
+        {
+            DataSet ds = new DataSet();
+            try
+            { 
+                SqlDataAdapter adapter = new SqlDataAdapter(queryString, conn);
+                adapter.Fill(ds);
+            }
+            catch (Exception ex)
+            {
+            }
+            return ds;
+        }
+
+
 
         protected void LinkButton1_Click(object sender, EventArgs e)
         {
